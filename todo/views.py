2fbc816160ed704bfb8b8b7e84566ed1b9bed13d
1F10250038 <s1f102500382@iniad.org>
2026-06-18 14:38:24 +0900

    ソート機能を追加

--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,7 @@ def index(request):
     tasks = Task.objects.all()
     
     context = {
-        tasks:tasks
+        'tasks':tasks
     }
     return render(request, 'todo/index.html', context)
 
