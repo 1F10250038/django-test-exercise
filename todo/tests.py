2527fa670790dab97f5077d47f345ff2cf564ec8
1F10250038 <s1f102500382@iniad.org>
2026-07-02 01:32:14 +0900

    flake8の警告を修正8

--- todo/tests.py
+++ todo/tests.py
@@ -45,11 +45,13 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertTrue(task.is_overdue(current))
+
     def test_is_overdue_none(self):
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
         task = Task(title='task1')
         task.save()
         self.assertFalse(task.is_overdue(current))
+
 
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
@@ -59,7 +61,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
 
-    def test_index_post (self):
+    def test_index_post(self):
         client = Client()
         data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data)
